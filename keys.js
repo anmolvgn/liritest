bc2ba62b7806e054c37e5cb353932eb13ffd720d
anmolvgn <anuggehalli93.com>
2017-10-01 21:08:04 -0600

    keys.js

--- keys.js
+++ keys.js
@@ -7,5 +7,12 @@ var twitterKeys = {
   access_token_secret: '<input here>',
 }
 
-module.exports = twitterKeys;
+var spotifyCreds = {
+    client_ID: '60f871aab1e744f69f2829752a386cb9',
+    client_secret: 'a6f0a02394d54855bb42c7ed029aba13',
+};
 
+module.exports = {
+  twitterKeys,
+  spotifyCreds,
+};
